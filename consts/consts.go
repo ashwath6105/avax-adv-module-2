5db33adda1ff9b758c1a89531f34e4e2cb736184
ashwath6105 <141135626+ashwath6105@users.noreply.github.com>
2023-09-19 20:14:35 +0530

    Update consts.go

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "CREATRIX"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "Ashwath"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "CTX"
 )
 
 var ID ids.ID
